26024db30b3a72b6a36bbdc523358e538c2dd8c5
moeabdol <mohd.a.saed@gmail.com>
2017-09-26 10:27:58 +0300

    Visualize oranges example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,56 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const canvas = d3.select('#canvas')
-      .append('svg')
-      .attr('viewBox', '0 0 100 100');
+    // const canvas = d3.select('#canvas')
+    //   .append('svg')
+    //   .attr('viewBox', '0 0 100 100');
       // .attr('width', 800)
       // .attr('height', 600);
 
-    const circle = canvas.append('circle')
-      .attr('cx', 5)
-      .attr('cy', 5)
-      .attr('r', 5)
-      .attr('fill', '#0000FF');
+    // const circle = canvas.append('circle')
+    //   .attr('cx', 5)
+    //   .attr('cy', 5)
+    //   .attr('r', 5)
+    //   .attr('fill', '#0000FF');
+    //
+    // const square = canvas.append('rect')
+    //   .attr('x', 10)
+    //   .attr('y', 0)
+    //   .attr('width', 10)
+    //   .attr('height', 10);
+    //
+    // const line = canvas.append('line')
+    //   .attr('x1', 0)
+    //   .attr('y1', 10)
+    //   .attr('x2', 10)
+    //   .attr('y2', 20)
+    //   .attr('stroke', 'grey')
+    //   .attr('stroke-width', 0.3);
 
-    const square = canvas.append('rect')
-      .attr('x', 10)
-      .attr('y', 0)
-      .attr('width', 10)
-      .attr('height', 10);
+    this.visualizeOranges();
+  }
 
-    const line = canvas.append('line')
-      .attr('x1', 0)
-      .attr('y1', 10)
-      .attr('x2', 10)
-      .attr('y2', 20)
-      .attr('stroke', 'grey')
-      .attr('stroke-width', 0.3);
+  visualizeOranges() {
+    const orangeDate = [10, 30, 50, 100];
+
+    const canvas = d3.select('#orange-container')
+      .append('svg')
+      .attr('width', 768)
+      .attr('height', 1024);
+
+    const oranges = canvas.selectAll('circle')
+      .data(orangeDate)
+      .enter()
+      .append('circle')
+      .attr('fill', 'orange')
+      .attr('cx', function (d, i) {
+        return d + (i * 100);
+      })
+      .attr('cy', function(d) {
+        return d;
+      })
+      .attr('r', function(d) {
+        return d;
+      });
   }
 }
