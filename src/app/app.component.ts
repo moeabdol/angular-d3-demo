09158e4c09d75ba21f246e266c2175a09ffbd982
moeabdol <mohd.a.saed@gmail.com>
2017-09-26 09:20:51 +0300

    Install d3js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    console.log(d3['version']);
+  }
 }
